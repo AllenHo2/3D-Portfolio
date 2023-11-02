ba14fabd228436e4cdd8dc5cc6418c5a97bde5fd
AllenHo2 <aho22@dons.usfca.edu>
2023-11-02 00:28:08 -0700

    Navbar and Introduction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen
+    mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#F9A8D4]'/>
+          <div className='w-1 sm:h-96 h-40 bg-gradient-to-b from-pink-300'/>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-sky-400`}> 
+          Hello, I'm <span className='text-[#F9A8D4]'> Allen </span></h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop backend applications <br className='sm:block hidden'/>
+            and websites
+          </p>
+        </div>
+
+
+        <ComputersCanvas />
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,11 @@ import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav
-    className = {`${styles.paddindX} w-full flex 
+    className = {`${styles.paddingX} w-full flex 
     items-center py-5 fixed top-0 z-20 bg-primary`}        
     >
 
@@ -25,8 +26,58 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Allen <span className='sm:block hidden'> Javascript Mastery</span></p>
+          <p className='text-white text-[18px] 
+          font-bold cursor-pointer flex'>
+            Allen &nbsp;
+            <span className='sm:block hidden'> 
+            | Javascript Mastery</span>
+          </p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((Link) => (
+            <li key={Link}
+              className={`${
+                active === Link.title
+                ? "text-white"
+                : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(Link.title)}
+            >
+              <a href={`#${Link.id}`}>{Link.title}</a>
+            </li>         
+          ))}
+        </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src ={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-28px]
+            object-contain cursor-pointer'
+              onClick={() => setToggle(!toggle)}
+          />
+
+              <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute 
+              top-20 right-0 mx-4 my-2 min-w-[140px] z-10 roudned-x1`}>
+              <ul className='list-none flex sm:flex flex-row justify-end items-start flex-col gap-4'>
+                {navLinks.map((Link) => (
+                  <li key={Link}
+                    className={`${
+                      active === Link.title
+                      ? "text-white"
+                      : "text-secondary"
+                    } font-poppins font-medium cursor-pointer text-[18px]`}
+                    onClick={() => {
+                      setToggle(!toggle);
+                      setActive(Link.title)
+                    }}
+                  >
+                    <a href={`#${Link.id}`}>{Link.title}</a>
+                  </li>         
+                ))}
+              </ul>
+              </div>
+        </div>
       </div>
 
 
